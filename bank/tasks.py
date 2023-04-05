5bd3effa31b47e2ed8a451bf7c80c1f4ce99f4ea
Petr (Laptop) <petrbohac3@seznam.cz>
2023-04-05 22:22:52 +0200

    added a management command for updating the exchange rates

--- bank/tasks.py
+++ bank/tasks.py
@@ -1,16 +1,7 @@
-from django.core.exceptions import ObjectDoesNotExist
-
 from STINBank.celery import app
-from bank.models import CurrencyRate
-from bank.utils.cnb import fetch_rates
+from bank.utils.cnb import update_rates
 
 
 @app.task
-def update_rates():
-    for currency_data in fetch_rates():
-        try:
-            currency_rate = CurrencyRate.objects.get(currency=currency_data.code)
-            currency_rate.rate = currency_data.rate
-        except ObjectDoesNotExist:
-            currency_rate = CurrencyRate(currency=currency_data.code, rate=currency_data.rate)
-        currency_rate.save()
+def update_rates_task():
+    update_rates()
--- bank/utils/cnb.py
+++ bank/utils/cnb.py
@@ -1,6 +1,8 @@
 import requests
+from django.core.exceptions import ObjectDoesNotExist
 
 from STINBank.utils.config import get_bank_config
+from bank.models import CurrencyRate
 
 
 class Currency:
@@ -11,6 +13,9 @@ class Currency:
         self.code = code
         self.rate = rate
 
+    def __str__(self) -> str:
+        return f'{self.currency} ({self.code})'
+
 
 def fetch_rates() -> tuple[Currency]:
     data = requests.get(get_bank_config().cnb_rates_url)
@@ -20,3 +25,25 @@ def fetch_rates() -> tuple[Currency]:
         data = line.split('|')
         rates.append(Currency(data[0], data[1], int(data[2]), data[3], float(data[4].replace(',', '.'))))
     return tuple(rates)
+
+
+def update_rates(debug: bool = False):
+    for currency_data in fetch_rates():
+        changes_made = False
+        if debug:
+            print(f'Processing {currency_data}')
+        try:
+            currency_rate = CurrencyRate.objects.get(currency=currency_data.code)
+            if currency_rate.rate != currency_data.rate:
+                if debug:
+                    print(f'Updating {currency_data.code} from {currency_rate.rate} to {currency_data.rate}...', end='')
+                currency_rate.rate = currency_data.rate
+                changes_made = True
+        except ObjectDoesNotExist:
+            if debug:
+                print(f'Creating a new CurrencyRate object for {currency_data} with the value {currency_data.rate}...')
+            currency_rate = CurrencyRate(currency=currency_data.code, rate=currency_data.rate)
+            changes_made = True
+        currency_rate.save()
+        if debug:
+            print('done' if changes_made else 'Currency up-to-date')
