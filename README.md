5bdbdb9c75a56f4cb04cdec1331168464b6e4984
Petr (Laptop) <petrbohac3@seznam.cz>
2023-04-05 23:54:36 +0200

    notes, dependencies

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Semestrální práce pro předmět STIN
 ## Features
 - [ ] 2 Factor Authentication
 - [ ] Multi-currency accounts
+
+## Notes for my forgtful ass
+- Celery
+  - run the worker => celery -A STINBank worker (-l INFO) (-P gevent - pro windejsy)
+  - run the scheduler => celery -A STINBank beat (-l INFO)
--- STINBank/settings.py
+++ STINBank/settings.py
@@ -35,15 +35,18 @@ AUTH_USER_MODEL = 'accounts.User'
 # Application definition
 
 INSTALLED_APPS = [
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # STINBank
     'STINBank',
     'bank',
     'accounts',
+    # Dependencies
     'fontawesomefree',
     'celery'
 ]
