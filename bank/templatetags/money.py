73c14ca159544b8603f8a3569487225530458c34
Petr (Desktop) <petrbohac3@seznam.cz>
2023-04-06 22:00:51 +0200

    fixed currency display for three specific currencies

--- bank/templatetags/money.py
+++ bank/templatetags/money.py
@@ -1,19 +1,20 @@
 from babel.numbers import format_currency
 from django import template
 
-
 register = template.Library()
 
 
-# TODO test cuz ne vsechny meny fungujou
 @register.inclusion_tag('includes/account_balance.html', takes_context=True)
 def account_balance(context, account):
     currency = context.request.user.get_preferred_currency()
     balance = format_currency(
         account.get_total_balance(currency), currency, format=u"#,##0.00 ¤", locale="cs_CZ"
-    ).split(',')
-    print(balance)
+    )
+    split_balance = balance.split(',' if ',' in balance else ' ')
+    balance_whole = split_balance[0]
+    balance_decimal = ('00 ' if ',' not in balance else '') + split_balance[1]
+
     return {
-        'balance_whole': balance[0],
-        'balance_decimal': balance[1]
+        'balance_whole': balance_whole,
+        'balance_decimal': balance_decimal
     }
--- bank/tests.py
+++ bank/tests.py
@@ -1,14 +1,1 @@
-from babel.numbers import format_currency
-from django.test import TestCase
-
-from bank.utils.currency import CURRENCIES
-
-
 # Create your tests here.
-
-def test_currency_format():
-    for currency in CURRENCIES.keys():
-        balance = format_currency(
-            10000, currency, format=u"#,##0.00 ¤", locale="cs_CZ"
-        ).split(',')
-        print(balance)
