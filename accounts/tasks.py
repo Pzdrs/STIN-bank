051f79d2112c7fbfcf646914f251e32c87104993
Petr (Desktop) <petrbohac3@seznam.cz>
2023-04-07 15:50:16 +0200

    config misto hardcodu

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -7,6 +7,7 @@ from decouple import config
 from django.conf import settings
 
 from STINBank.celery import app
+from STINBank.utils.config import get_project_config
 from accounts.models import User
 
 
@@ -15,7 +16,7 @@ def generate_qr_code(user_id: int):
     user = User.objects.get(pk=user_id)
     uri = pyotp.totp.TOTP(config('TOTP_KEY')).provisioning_uri(
         name=user.username,
-        issuer_name='STINBank'
+        issuer_name=get_project_config().name
     )
     Path(os.path.join(settings.MEDIA_ROOT, 'qr_codes')).mkdir(parents=True, exist_ok=True)
     qrcode.make(uri).save(os.path.join(settings.MEDIA_ROOT, 'qr_codes', f'{user.pk}.png'))
