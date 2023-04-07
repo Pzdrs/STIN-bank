5ae3d89ebcc4b63dffa9fbbef6ad5b81b615b0a8
Petr (Desktop) <petrbohac3@seznam.cz>
2023-04-07 13:05:07 +0200

    more robust verification form, admin added option to toggle 2fa

--- STINBank/static/js/totp_verification.js
+++ STINBank/static/js/totp_verification.js
@@ -1,4 +1,13 @@
 const input = document.querySelector('input[name=code]');
+input.addEventListener('keydown', ev => {
+    if (!/\d/.test(ev.key) && ev.key !== 'Backspace') {
+        ev.preventDefault();
+    }
+});
+
 input.addEventListener('input', ev => {
-    if (ev.target.value.length === 6) input.form.submit()
+    ev.target.value = ev.target.value.replace(/\D/g, '');
+    if (ev.target.value.length === 6) {
+        input.form.submit();
+    }
 });
--- accounts/admin.py
+++ accounts/admin.py
@@ -7,11 +7,12 @@ from accounts.models import User
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
-    list_display = ("username", "email", "first_name", "last_name", "is_staff", "pending_verification")
+    list_display = ("username", "email", "first_name", "last_name", "is_staff", "use_2fa", "pending_verification")
 
     def get_fieldsets(self, request, obj=None):
         fieldsets = list(super().get_fieldsets(request, obj))
-        fieldsets.append(
+        fieldsets.extend((
             (_("Bank related data"), {"fields": ("preferred_currency",)}),
-        )
+            (_("Security"), {"fields": ("use_2fa",)}),
+        ))
         return fieldsets
