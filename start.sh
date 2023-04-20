8d6a197d81c4b08d31a2eec366736d0a6dd6c041
Petr (Laptop) <petrbohac3@seznam.cz>
2023-04-20 09:55:41 +0200

    docker

--- start.sh
+++ start.sh
@@ -1,3 +1,1 @@
-python manage.py runserver 0.0.0.0:8000
-celery -A STINBank worker -l INFO
-celery -A STINBank beat -l INFO
+python manage.py runserver 0.0.0.0:8000 && celery -A STINBank worker -l INFO; celery -A STINBank beat -l INFO
